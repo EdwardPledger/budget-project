9f1b096a7105cea10a1451325289daf456a219aa
Edward Pledger <edward.l.pledger@gmail.com>
2022-05-02 15:25:16 -0500

    Basic login feature

--- src/apis/UserApis.ts
+++ src/apis/UserApis.ts
@@ -2,6 +2,22 @@ import User from '../interfaces/User'
 import { supabase } from '../helpers/supabase'
 
 class UserApis {
+  static getUser = async (username: string) => {
+    try {
+      const response = await supabase
+        .from<User>('user_account')
+        .select('username, user_password')
+        .match({ username: username })
+
+      if (response.error) throw response.error
+      else return response.data[0]
+
+    } catch (error) {
+      console.log(error)
+      alert(error)
+    }
+  }
+
   static insertUser = async (user: User) => {
     try {
       const response = await supabase
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,43 @@
 <template>
-  hi
+  <Form>
+    <template #header>
+      <h2>Login</h2>
+    </template>
+    <template #main>
+      <it-input labelTop="Username" type="text" required v-model="username" />
+      <it-input labelTop="Password" type="text" required v-model="password" />
+    </template>
+    <template #footer>
+      <it-button id="submit-button" type="success" round @click="loginUser">Submit</it-button>
+    </template>
+  </Form>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import Form from './Form.vue'
+import UserApis from '../apis/UserApis'
+import router from '../router'
 
 export default defineComponent({
+  components: { Form },
+  async setup() {
+    var username = ref<string>("")
+    var password = ref<string>("")
 
+    const loginUser = async () => {
+      const fetchedUser = await UserApis.getUser(username.value)
+
+      if (fetchedUser == null || fetchedUser?.user_password != password.value) {
+        alert('Username and/or password incorrect')
+      }
+      else {
+        router.push('/')
+      }
+    }
+
+    return { username, password, loginUser }
+  }
 })
 </script>
 
